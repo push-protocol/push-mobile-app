ab1f64b37305946eaf970d3ee598834aff75fa1f
Rayan <itsrayaaan@gmail.com>
2021-09-30 19:06:10 +0530

    added channel discovery feature

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import AppBadgeHelper from "./src/helpers/AppBadgeHelper";
 import messaging from "@react-native-firebase/messaging";
 
 import SplashScreen from "src/screens/SplashScreen";
@@ -22,6 +22,7 @@ import SignInScreenAdvance from "src/screens/SignInScreenAdvance";
 import BiometricScreen from "src/screens/BiometricScreen";
 import PushNotifyScreen from "src/screens/PushNotifyScreen";
 import SetupCompleteScreen from "src/screens/SetupCompleteScreen";
+import Tabs from "./src/screens/Tabs";
 
 import MetaStorage from "src/singletons/MetaStorage";
 import Notify from "src/singletons/Notify";
@@ -29,236 +30,267 @@ import Notify from "src/singletons/Notify";
 import AuthContext, { APP_AUTH_STATES } from "src/components/auth/AuthContext";
 import ENV_CONFIG from "src/env.config";
 import GLOBALS from "src/Globals";
+import OnboardingChannel from "./src/screens/OnboardingChannel";
 
 // Assign console.log to nothing
 if (!ENV_CONFIG.SHOW_CONSOLE) {
-	console.log("Production Environment... disabling console!");
-	console.log = () => {};
+  console.log("Production Environment... disabling console!");
+  // console.log = () => {};
 }
 
 // Create Stack Navigator
 const Stack = createStackNavigator();
 
 export default function App({ navigation }) {
-	// State Settings
-	// VALID APP AUTH STATES
-	const [appAuthState, setAppAuthState] = useState(
-		APP_AUTH_STATES.INITIALIZING
-	);
-	const [userWallet, setUserWallet] = useState("");
-	const [userPKey, setUserPKey] = useState("");
+  // State Settings
+  // VALID APP AUTH STATES
+  const [appAuthState, setAppAuthState] = useState(
+    APP_AUTH_STATES.INITIALIZING
+  );
+  const [userWallet, setUserWallet] = useState("");
+  const [userPKey, setUserPKey] = useState("");
 
-	// HANDLE ON APP START
-	React.useEffect(() => {
-		// PUSH NOTIFICATIONS HANDLING
-		// Request Device Token and save it user is signed in
-		Notify.instance.requestDeviceToken(true);
+  // HANDLE ON APP START
+  React.useEffect(() => {
+    // PUSH NOTIFICATIONS HANDLING
+    // Request Device Token and save it user is signed in
+    Notify.instance.requestDeviceToken(true);
 
-		// Listen to whether the token changes
-		const onTokenRefresh = messaging().onTokenRefresh((token) => {
-			Notify.instance.saveDeviceToken(token, true); // true means it's a refresh
-		});
+    // Listen to whether the token changes
+    const onTokenRefresh = messaging().onTokenRefresh((token) => {
+      Notify.instance.saveDeviceToken(token, true); // true means it's a refresh
+    });
 
-		// Listen for incoming messages
-		const handleForegroundPush = messaging().onMessage(
-			async (remoteMessage) => {
-				Notify.instance.handleIncomingPushAppOpened(remoteMessage);
-			}
-		);
+    // Listen for incoming messages
+    const handleForegroundPush = messaging().onMessage(
+      async (remoteMessage) => {
+        Notify.instance.handleIncomingPushAppOpened(remoteMessage);
+      }
+    );
 
-		messaging().onNotificationOpenedApp((remoteMessage) => {
-			Notify.instance.triggerNotificationListenerCallback();
-		});
+    messaging().onNotificationOpenedApp((remoteMessage) => {
+      Notify.instance.triggerNotificationListenerCallback();
+    });
 
-		messaging()
-			.getInitialNotification()
-			.then((remoteMessage) => {
-				if (remoteMessage) {
-					Notify.instance.triggerNotificationListenerCallback();
-				}
-			});
+    messaging()
+      .getInitialNotification()
+      .then((remoteMessage) => {
+        if (remoteMessage) {
+          Notify.instance.triggerNotificationListenerCallback();
+        }
+      });
 
-		return () => {
-			onTokenRefresh;
-			handleForegroundPush;
-		};
-	}, []);
+    return () => {
+      onTokenRefresh;
+      handleForegroundPush;
+      handleAppNotificationBadge();
+    };
+  }, []);
 
-	React.useEffect(() => {
-		//
-		// messaging()
-		//   .getInitialNotification()
-		//   .then(remoteMessage => {
-		//     if (remoteMessage) {
-		//       Notify.instance.triggerNotificationListenerCallback();
-		//     }
-		//   });
-	}, []);
+  React.useEffect(() => {
+    //
+    // messaging()
+    //   .getInitialNotification()
+    //   .then(remoteMessage => {
+    //     if (remoteMessage) {
+    //       Notify.instance.triggerNotificationListenerCallback();
+    //     }
+    //   });
+  }, []);
 
-	// HANDLE AUTH FLOW
-	const authContext = React.useMemo(
-		() => ({
-			handleAppAuthState: (newAuthState, wallet, pkey) => {
-				setUserWallet(wallet);
-				setUserPKey(pkey);
-				setAppAuthState(newAuthState);
-			},
-		}),
-		[]
-	);
+  const handleAppNotificationBadge = async () => {
+    await AppBadgeHelper.setAppBadgeCount(0);
+  };
 
-	// RENDER ASSIST
-	renderSelectiveScreens = () => {
-		// User is Authenticated
-		if (appAuthState == APP_AUTH_STATES.AUTHENTICATED) {
-			return (
-				<React.Fragment>
-					<Stack.Screen
-						name="Home"
-						component={HomeScreen}
-						options={{
-							headerShown: false,
-						}}
-						initialParams={{
-							wallet: userWallet,
-							pkey: userPKey,
-						}}
-					/>
+  // HANDLE AUTH FLOW
+  const authContext = React.useMemo(
+    () => ({
+      handleAppAuthState: (newAuthState, wallet, pkey) => {
+        setUserWallet(wallet);
+        setUserPKey(pkey);
+        setAppAuthState(newAuthState);
+      },
+    }),
+    []
+  );
 
-					<Stack.Screen
-						name="Settings"
-						component={SettingsScreen}
-						options={{
-							title: "Settings",
-							headerStyle: {
-								backgroundColor: GLOBALS.COLORS.WHITE,
-							},
-							headerTintColor: GLOBALS.COLORS.MID_GRAY,
-						}}
-					/>
+  // RENDER ASSIST
+  renderSelectiveScreens = () => {
+    // User is Authenticated
+    if (appAuthState == APP_AUTH_STATES.AUTHENTICATED) {
+      return (
+        <React.Fragment>
+          {/* <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              headerShown: false,
+            }}
+            initialParams={{
+              wallet: userWallet,
+              pkey: userPKey,
+            }}
+          /> */}
 
-					<Stack.Screen
-						name="SampleFeed"
-						component={SampleFeed}
-						options={{
-							title: "Example Feed",
-							headerStyle: {
-								backgroundColor: GLOBALS.COLORS.WHITE,
-							},
-							headerTintColor: GLOBALS.COLORS.MID_GRAY,
-						}}
-					/>
-				</React.Fragment>
-			);
-		}
-		// User is logging in
-		else if (appAuthState == APP_AUTH_STATES.ONBOARDING) {
-			return (
-				<React.Fragment>
-					<Stack.Screen
-						name="Welcome"
-						component={WelcomeScreen}
-						options={{
-							headerShown: false,
-							headerTintColor: GLOBALS.COLORS.MID_GRAY,
-						}}
-					/>
+          <Stack.Screen
+            name="Tabs"
+            component={Tabs}
+            options={{
+              headerShown: false,
+            }}
+            initialParams={{
+              wallet: userWallet,
+              pkey: userPKey,
+            }}
+          />
 
-					<Stack.Screen
-						name="SignIn"
-						component={SignInScreen}
-						options={{
-							headerShown: false,
-							headerTintColor: GLOBALS.COLORS.MID_GRAY,
-						}}
-					/>
+          <Stack.Screen
+            name="Settings"
+            component={SettingsScreen}
+            options={{
+              title: "Settings",
+              headerStyle: {
+                backgroundColor: GLOBALS.COLORS.WHITE,
+              },
+              headerTintColor: GLOBALS.COLORS.MID_GRAY,
+            }}
+          />
 
-					<Stack.Screen
-						name="SignInAdvance"
-						component={SignInScreenAdvance}
-						options={{
-							headerShown: false,
-							headerTintColor: GLOBALS.COLORS.MID_GRAY,
-						}}
-					/>
+          <Stack.Screen
+            name="SampleFeed"
+            component={SampleFeed}
+            options={{
+              title: "Example Feed",
+              headerStyle: {
+                backgroundColor: GLOBALS.COLORS.WHITE,
+              },
+              headerTintColor: GLOBALS.COLORS.MID_GRAY,
+            }}
+          />
+        </React.Fragment>
+      );
+    }
+    // User is logging in
+    else if (appAuthState == APP_AUTH_STATES.ONBOARDING) {
+      return (
+        <React.Fragment>
+          <Stack.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{
+              headerShown: false,
+              headerTintColor: GLOBALS.COLORS.MID_GRAY,
+            }}
+          />
 
-					<Stack.Screen
-						name="Biometric"
-						component={BiometricScreen}
-						options={{
-							headerShown: false,
-							headerTintColor: GLOBALS.COLORS.MID_GRAY,
-						}}
-					/>
+          <Stack.Screen
+            name="SignIn"
+            component={SignInScreen}
+            options={{
+              headerShown: false,
+              headerTintColor: GLOBALS.COLORS.MID_GRAY,
+            }}
+          />
 
-					<Stack.Screen
-						name="PushNotify"
-						component={PushNotifyScreen}
-						options={{
-							headerShown: false,
-							headerTintColor: GLOBALS.COLORS.MID_GRAY,
-						}}
-					/>
+          <Stack.Screen
+            name="SignInAdvance"
+            component={SignInScreenAdvance}
+            options={{
+              headerShown: false,
+              headerTintColor: GLOBALS.COLORS.MID_GRAY,
+            }}
+          />
 
-					<Stack.Screen
-						name="SetupComplete"
-						component={SetupCompleteScreen}
-						options={{
-							headerShown: false,
-							headerTintColor: GLOBALS.COLORS.MID_GRAY,
-						}}
-					/>
-				</React.Fragment>
-			);
-		}
-		// App is loading or User is getting authenticated
-		else if (
-			appAuthState == APP_AUTH_STATES.INITIALIZING ||
-			appAuthState == APP_AUTH_STATES.ONBOARDED
-		) {
-			return (
-				<Stack.Screen
-					name="Splash"
-					component={SplashScreen}
-					options={{
-						headerTransparent: true,
-					}}
-				/>
-			);
-		}
-	};
+          <Stack.Screen
+            name="Biometric"
+            component={BiometricScreen}
+            options={{
+              headerShown: false,
+              headerTintColor: GLOBALS.COLORS.MID_GRAY,
+            }}
+          />
 
-	// RENDER
-	return (
-		<AuthContext.Provider value={authContext}>
-			<SafeAreaProvider>
-				<NavigationContainer>
-					<Stack.Navigator
-						screenOptions={{
-							title: "",
-							headerStyle: {
-								backgroundColor: GLOBALS.COLORS.WHITE,
-								shadowColor: "transparent",
-								shadowRadius: 0,
-								shadowOffset: {
-									height: 0,
-								},
-								elevation: 0,
-							},
-							headerTitleStyle: {
-								color: GLOBALS.COLORS.BLACK,
-							},
-							headerBackTitleStyle: {
-								color: GLOBALS.COLORS.PRIMARY,
-							},
-							headerTintColor: GLOBALS.COLORS.BLACK,
-							headerTitleAlign: "center",
-							headerBackTitleVisible: false,
-						}}>
-						{renderSelectiveScreens()}
-					</Stack.Navigator>
-				</NavigationContainer>
-			</SafeAreaProvider>
-		</AuthContext.Provider>
-	);
+          <Stack.Screen
+            name="PushNotify"
+            component={PushNotifyScreen}
+            options={{
+              headerShown: false,
+              headerTintColor: GLOBALS.COLORS.MID_GRAY,
+            }}
+          />
+
+          {/* <Stack.Screen
+            name="OnboardingChannel"
+            component={OnboardingChannel}
+            options={{
+              headerShown: false,
+            }}
+            initialParams={{
+              wallet: userWallet,
+              pkey: userPKey,
+            }}
+          /> */}
+
+          <Stack.Screen
+            name="SetupComplete"
+            component={SetupCompleteScreen}
+            options={{
+              headerShown: false,
+              headerTintColor: GLOBALS.COLORS.MID_GRAY,
+            }}
+          />
+        </React.Fragment>
+      );
+    }
+    // App is loading or User is getting authenticated
+    else if (
+      appAuthState == APP_AUTH_STATES.INITIALIZING ||
+      appAuthState == APP_AUTH_STATES.ONBOARDED
+    ) {
+      return (
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{
+            headerTransparent: true,
+          }}
+        />
+      );
+    }
+  };
+
+  // RENDER
+  return (
+    <AuthContext.Provider value={authContext}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              title: "",
+              headerStyle: {
+                backgroundColor: GLOBALS.COLORS.WHITE,
+                shadowColor: "transparent",
+                shadowRadius: 0,
+                shadowOffset: {
+                  height: 0,
+                },
+                elevation: 0,
+              },
+              headerTitleStyle: {
+                color: GLOBALS.COLORS.BLACK,
+              },
+              headerBackTitleStyle: {
+                color: GLOBALS.COLORS.PRIMARY,
+              },
+              headerTintColor: GLOBALS.COLORS.BLACK,
+              headerTitleAlign: "center",
+              headerBackTitleVisible: false,
+            }}
+          >
+            {renderSelectiveScreens()}
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </AuthContext.Provider>
+  );
 }
