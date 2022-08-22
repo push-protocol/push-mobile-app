df479bf8d091e24ec90726c1c622d6d135113580
harshrajat <harsh@epns.io>
2022-08-22 20:23:01 +0400

    Rollback to expo 39

--- android/app/src/main/java/io/epns/epnsstaging/generated/BasePackageList.java
+++ android/app/src/main/java/io/epns/epnsstaging/generated/BasePackageList.java
@@ -22,9 +22,9 @@ public class BasePackageList {
         new expo.modules.location.LocationPackage(),
         new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.sharing.SharingPackage(),
+        new expo.modules.splashscreen.SplashScreenPackage(),
         new expo.modules.sqlite.SQLitePackage(),
-        new expo.modules.updates.UpdatesPackage(),
-        new expo.modules.webbrowser.WebBrowserPackage()
+        new expo.modules.updates.UpdatesPackage()
     );
   }
 }
