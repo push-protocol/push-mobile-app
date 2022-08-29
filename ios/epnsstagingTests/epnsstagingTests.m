a2d6533ab6bdaaf4bbfe313d36a2621151ec73c0
harshrajat <harsh@epns.io>
2022-08-30 01:52:31 +0400

    Updated iOS things

--- ios/epnsstagingTests/epnsstagingTests.m
+++ ios/epnsstagingTests/epnsstagingTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface epns_mobile_appTests : XCTestCase
+@interface epnsstagingTests : XCTestCase
 
 @end
 
-@implementation epns_mobile_appTests
+@implementation epnsstagingTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
