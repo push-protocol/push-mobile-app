68ea0852d1f168d282bd9f2b69c7ea4c097480ad
Abishek Bashyal <abishek_bashyal@outlook.com>
2022-09-12 17:55:43 +0545

    remove tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   overrides: [
     {
-      files: ['*.ts', '*.tsx', '*.js'],
+      files: ['*.ts', '*.tsx', '*.js', '*.tsx'],
       rules: {
         '@typescript-eslint/no-shadow': ['error'],
         'no-shadow': 'off',
