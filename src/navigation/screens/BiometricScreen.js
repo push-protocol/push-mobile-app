f6fe2093d0aa06fc3e5806fdbce26ea194a9c88f
Abishek Bashyal <abishek_bashyal@outlook.com>
2022-09-19 01:10:08 +0545

    signin fix

--- src/navigation/screens/BiometricScreen.js
+++ src/navigation/screens/BiometricScreen.js
@@ -37,7 +37,6 @@ function ScreenFinishedTransition({setScreenTransitionAsDone}) {
       });
 
       return () => task.cancel();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []),
   );
 
@@ -284,7 +283,6 @@ class BiometricScreen extends Component {
     // Check if biometric is present, if so present authentication
     // If authenticated, store the passcode on secure chain
     if (this.state.biometricSupported) {
-      console.log('click me');
       let biometricType = 'Null';
 
       if (
@@ -342,7 +340,6 @@ class BiometricScreen extends Component {
         );
       }
     } else {
-      console.log('no bimertric me');
       this.loadNextScreen();
     }
   };
@@ -352,13 +349,9 @@ class BiometricScreen extends Component {
   };
 
   loadNextScreen = async () => {
-    // TODO:fix
-    this.props.navigation.navigate(GLOBALS.SCREENS.SETUPCOMPLETE);
-
     // Goto Next Screen
     // Check if the push notification permission is waiting for first grant
     // If not, skip this step completely as user either gave permission or denied it
-    // FIREBASE
     const authorizationStatus = await messaging().hasPermission();
 
     if (authorizationStatus == messaging.AuthorizationStatus.NOT_DETERMINED) {
