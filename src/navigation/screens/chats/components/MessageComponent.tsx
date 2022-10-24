1ddacfab6b9d377f057eb0441d47501ddbabe54d
Abishek Bashyal <abishek_bashyal@outlook.com>
2022-10-25 01:00:02 +0545

    bifurcation of all chattypes components done

--- src/navigation/screens/chats/components/MessageComponent.tsx
+++ src/navigation/screens/chats/components/MessageComponent.tsx
@@ -1,71 +1,15 @@
 import React from 'react';
-import {Image, Linking, StyleSheet, Text, View} from 'react-native';
-import {TouchableOpacity} from 'react-native-gesture-handler';
-import SvgUri from 'react-native-svg-uri';
-import Globals from 'src/Globals';
+import {StyleSheet, View} from 'react-native';
 
 import {ChatMessage} from '../helpers/chatResolver';
+import {FileMessageComponent, TextMessage} from './messageTypes';
+import {ImageMessage} from './messageTypes/ImageMessage';
 
-type MessageComponentType = 'SENDER' | 'RECEIVER';
+export type MessageComponentType = 'SENDER' | 'RECEIVER';
 
 type MessageComponentProps = {
   chatMessage: ChatMessage;
   componentType: MessageComponentType;
-};
-
-export interface FileMessageContent {
-  content: string;
-  name: string;
-  type: string;
-  size: number;
-}
-
-export const FILE_ICON = (extension: string) =>
-  `https://cdn.jsdelivr.net/gh/napthedev/file-icons/file/${extension}.svg`;
-
-export const formatFileSize = (size: number): string => {
-  const i = Math.floor(Math.log(size) / Math.log(1024));
-  return `${(size / Math.pow(1024, i)).toFixed(1)} ${
-    ['B', 'KB', 'MB', 'GB', 'TB'][i]
-  }`;
-};
-
-const FileMessageComponent = ({file}: {file: string}) => {
-  console.log('****This was called');
-
-  const fileContent: FileMessageContent = JSON.parse(file);
-  const name = fileContent.name;
-  let modifiedName: string;
-  if (name.length > 11) {
-    modifiedName = name.slice(0, 11) + '...';
-  } else {
-    modifiedName = name;
-  }
-  const content = fileContent.content as string;
-  const size = fileContent.size;
-
-  console.log('*********got', size, name, modifiedName);
-
-  console.log('got url', FILE_ICON(name.split('.').slice(-1)[0]));
-
-  return (
-    <View>
-      <View>
-        <SvgUri
-          source={{uri: FILE_ICON(name.split('.').slice(-1)[0])}}
-          width="100%"
-          height="40"
-        />
-      </View>
-      <View>
-        <Text>{modifiedName}</Text>
-        <Text>{formatFileSize(size)}</Text>
-      </View>
-      <TouchableOpacity onPress={() => Linking.openURL(content)}>
-        <Text>Download</Text>
-      </TouchableOpacity>
-    </View>
-  );
 };
 
 const MessageComponent = ({
@@ -73,32 +17,28 @@ const MessageComponent = ({
   componentType,
 }: MessageComponentProps) => {
   const styles = componentType === 'SENDER' ? SenderStyle : RecipientStyle;
-  const {message, messageType, time} = chatMessage;
+  const {message, messageType} = chatMessage;
 
   return (
     <View style={styles.container}>
-      <View style={styles.content}>
-        {messageType === 'GIF' && (
-          <Image
-            source={{
-              uri: message,
-            }}
-            style={{width: '100%', height: 120}}
-          />
-        )}
-        {messageType === 'Image' && (
-          <Image
-            source={{
-              uri: JSON.parse(message).content,
-            }}
-            style={{width: '100%', height: 120}}
-          />
-        )}
-        {messageType === 'Text' && <Text style={styles.text}>{message}</Text>}
-        {messageType === 'File' && <FileMessageComponent file={message} />}
-      </View>
-
-      <Text style={styles.time}>{time}</Text>
+      {messageType === 'GIF' && (
+        <ImageMessage
+          imageSource={chatMessage.message}
+          time={chatMessage.time}
+        />
+      )}
+      {messageType === 'Image' && (
+        <ImageMessage
+          imageSource={JSON.parse(message).content}
+          time={chatMessage.time}
+        />
+      )}
+      {messageType === 'Text' && (
+        <TextMessage chatMessage={chatMessage} componentType={componentType} />
+      )}
+      {messageType === 'File' && (
+        <FileMessageComponent chatMessage={chatMessage} />
+      )}
     </View>
   );
 };
@@ -107,47 +47,14 @@ export default MessageComponent;
 
 const RecipientStyle = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
-    maxWidth: '75%',
+    alignSelf: 'flex-start',
     marginBottom: 25,
-    padding: 15,
-    borderTopLeftRadius: 0,
-    borderTopRightRadius: 15,
-    borderBottomRightRadius: 15,
-    borderBottomLeftRadius: 20,
   },
-  content: {
-    marginBottom: 5,
-  },
-
-  text: {
-    fontSize: 14,
-    color: Globals.COLORS.BLACK,
-    fontWeight: '400',
-  },
-  time: {fontSize: 13, textAlign: 'right', color: '#6F829E'},
 });
 
 const SenderStyle = StyleSheet.create({
   container: {
-    backgroundColor: Globals.COLORS.PINK,
-    width: '75%',
-    marginBottom: 25,
-    padding: 15,
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 0,
-    borderBottomRightRadius: 15,
-    borderBottomLeftRadius: 20,
     alignSelf: 'flex-end',
+    marginBottom: 25,
   },
-  content: {
-    marginBottom: 5,
-  },
-  text: {
-    fontSize: 14,
-    color: 'white',
-    fontWeight: '400',
-    marginBottom: 5,
-  },
-  time: {fontSize: 13, textAlign: 'right', color: 'white'},
 });
