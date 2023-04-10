58c93571e648909c61b08fdb2f5b2cda85cadf5f
Abishek Bashyal <abishek_bashyal@outlook.com>
2023-04-10 18:51:43 +0545

    firebase notification test done

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
 import WalletConnectProvider from '@walletconnect/react-native-dapp';
 import React, {useEffect} from 'react';
+import {Text, Touchable, TouchableOpacity, View} from 'react-native';
+import RNCallKeep from 'react-native-callkeep';
 import 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import WebviewCrypto from 'react-native-webview-crypto';
@@ -17,6 +19,29 @@ import Notify from 'src/singletons/Notify';
 import appConfig from './app.json';
 
 let persistor = persistStore(store);
+
+const options = {
+  ios: {
+    appName: 'My app name',
+  },
+  android: {
+    alertTitle: 'Permissions required',
+    alertDescription: 'This application needs to access your phone accounts',
+    cancelButton: 'Cancel',
+    okButton: 'ok',
+    imageName: 'phone_account_icon',
+    // additionalPermissions: [PermissionsAndroid.PERMISSIONS.example],
+    // Required to get audio in background when using Android 11
+    foregroundService: {
+      channelId: 'com.company.my',
+      channelName: 'Foreground service for my app',
+      notificationTitle: 'My app is running on background',
+      notificationIcon: 'Path to the resource icon of the notification',
+    },
+  },
+};
+
+RNCallKeep.setup(options).then(accepted => {});
 
 const App = () => {
   const handleAppNotificationBadge = async () => {
@@ -35,6 +60,7 @@ const App = () => {
 
     const handleBackgroundMessageHandler =
       messaging().setBackgroundMessageHandler(async remoteMessage => {
+        await handleCall();
         await NotifeeDisplayNotification(remoteMessage);
       });
 
@@ -44,6 +70,40 @@ const App = () => {
       handleAppNotificationBadge();
     };
   }, []);
+
+  const handleCall = async () => {
+    RNCallKeep.displayIncomingCall(
+      'uid',
+      '0x85c58...6915BE',
+      'foo.eth',
+      'generic',
+      true,
+      {
+        handleType: 'generic',
+        localizedCallerName: 'eoo.eth',
+        foregroundColor: '#000000',
+        backgroundColor: '#FFFFFF',
+        notificationTitle: 'Incoming Call',
+        notificationBody: 'You have a new call',
+      },
+    );
+  };
+
+  // const test = true;
+  const test = false;
+
+  if (test) {
+    return (
+      <View>
+        <Text>Wola</Text>
+        <TouchableOpacity
+          onPress={handleCall}
+          style={{backgroundColor: 'green', width: 200, margin: 50}}>
+          <Text style={{padding: 20, color: 'white'}}>Do call</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 
   return (
     <SafeAreaProvider>
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import messaging from '@react-native-firebase/messaging';
 import React, {useEffect} from 'react';
 import {AppRegistry} from 'react-native';
+import RNCallKeep from 'react-native-callkeep';
 import 'react-native-crypto';
 import 'react-native-get-random-values';
 import WebviewCrypto from 'react-native-webview-crypto';
@@ -11,16 +12,59 @@ import App from './App';
 import {name as appName} from './app.json';
 import './shim';
 
+const options = {
+  ios: {
+    appName: 'My app name',
+  },
+  android: {
+    alertTitle: 'Permissions required',
+    alertDescription: 'This application needs to access your phone accounts',
+    cancelButton: 'Cancel',
+    okButton: 'ok',
+    imageName: 'phone_account_icon',
+    // additionalPermissions: [PermissionsAndroid.PERMISSIONS.example],
+    // Required to get audio in background when using Android 11
+    foregroundService: {
+      channelId: 'com.company.my',
+      channelName: 'Foreground service for my app',
+      notificationTitle: 'My app is running on background',
+      notificationIcon: 'Path to the resource icon of the notification',
+    },
+  },
+};
+
+const handleCall = async () => {
+  RNCallKeep.displayIncomingCall(
+    'uid',
+    '0x85c58...6915BE',
+    'foo.eth',
+    'generic',
+    true,
+    {
+      handleType: 'generic',
+      localizedCallerName: 'eoo.eth',
+      foregroundColor: '#000000',
+      backgroundColor: '#FFFFFF',
+      notificationTitle: 'Incoming Call',
+      notificationBody: 'You have a new call',
+    },
+  );
+};
+
 // FIREBASE
 // Register background handler
 messaging().setBackgroundMessageHandler(async remoteMessage => {
-  await NotifeeDisplayNotification(remoteMessage);
+  console.log('I was called');
+  await handleCall();
+  // await NotifeeDisplayNotification(remoteMessage);
 });
 
 function HeadlessCheck({isHeadless}) {
   useEffect(() => {
     NotifeClearBadge();
   }, []);
+
+  RNCallKeep.setup(options).then(accepted => {});
 
   if (isHeadless) {
     // App has been launched in the background by iOS, ignore
@@ -31,3 +75,12 @@ function HeadlessCheck({isHeadless}) {
 }
 
 AppRegistry.registerComponent(appName, () => HeadlessCheck);
+AppRegistry.registerHeadlessTask(
+  'RNCallKeepBackgroundMessage',
+  () =>
+    ({name, callUUID, handle}) => {
+      // Make your call here
+
+      return Promise.resolve();
+    },
+);
