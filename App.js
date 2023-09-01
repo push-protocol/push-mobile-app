0780340e052ad6e9fe2b3f3915139b98fa237c01
kalashshah <202051096@iiitvadodara.ac.in>
2023-09-01 16:24:56 +0530

    refactor: custom hook for notifications

--- App.js
+++ App.js
@@ -1,126 +1,24 @@
-import {VideoCallStatus} from '@pushprotocol/restapi';
-import messaging from '@react-native-firebase/messaging';
 import {WalletConnectModal} from '@walletconnect/modal-react-native';
 import '@walletconnect/react-native-compat';
-import React, {useCallback, useEffect, useState} from 'react';
-import {Platform} from 'react-native';
-import RNCallKeep from 'react-native-callkeep';
+import React from 'react';
 import 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import VoipPushNotification from 'react-native-voip-push-notification';
 import WebviewCrypto from 'react-native-webview-crypto';
 import {Provider} from 'react-redux';
 import {persistStore} from 'redux-persist';
 import {PersistGate} from 'redux-persist/integration/react';
 import VideoCallContextProvider from 'src/contexts/VideoContext';
-import AppBadgeHelper from 'src/helpers/AppBadgeHelper';
 import AppScreens from 'src/navigation';
+import {useNotification} from 'src/push_video/hooks/useNotification';
 import store from 'src/redux';
-import MetaStorage from 'src/singletons/MetaStorage';
-import Notify from 'src/singletons/Notify';
 import {WalletConnectConfig} from 'src/walletconnect';
 
 import appConfig from './app.json';
-import CallkeepHelper from './src/callkeep';
 
 let persistor = persistStore(store);
 
-const handleAppNotificationBadge = async () => {
-  await AppBadgeHelper.setAppBadgeCount(0);
-};
-
 const App = ({isCallAccepted}) => {
-  const [numBgCallAccepted, setNumBgCallAccepted] = useState(0);
-  useEffect(() => {
-    // PUSH NOTIFICATIONS HANDLING
-    Notify.instance.requestDeviceToken(true);
-
-    // Listen to whether the token changes
-    const onTokenRefresh = messaging().onTokenRefresh(token => {
-      Notify.instance.saveDeviceToken(token, true); // true means it's a refresh
-    });
-
-    return () => {
-      onTokenRefresh;
-      handleAppNotificationBadge();
-    };
-  }, []);
-
-  const answerIncomingCall = useCallback(() => {
-    MetaStorage.instance.setBackgroundCallAccepted(false);
-    setNumBgCallAccepted(prev => prev + 1);
-  }, []);
-
-  const endIncomingCall = useCallback(() => {
-    CallkeepHelper.endAllCall();
-  }, []);
-
-  // this is supposed to be called when app is at bg
-  // android only
-  useEffect(() => {
-    RNCallKeep.addEventListener('answerCall', async ({callUUID}) => {
-      if (Platform.OS === 'android') {
-        CallkeepHelper.backToForeground();
-        CallkeepHelper.endAllCall();
-      }
-      answerIncomingCall();
-    });
-
-    RNCallKeep.addEventListener('endCall', async ({callUUID}) => {
-      endIncomingCall();
-    });
-
-    RNCallKeep.addEventListener('didLoadWithEvents', events => {
-      console.log('didLoadWithEvents', events);
-      const answerCallEvent = events.find(
-        event => event.name === 'RNCallKeepPerformAnswerCallAction',
-      );
-      console.log('answerCallEvent', answerCallEvent);
-      if (answerCallEvent) {
-        answerIncomingCall();
-      }
-    });
-  }, []);
-
-  useEffect(() => {
-    VoipPushNotification.addEventListener('register', token => {
-      console.log('Registering APN token:', token);
-    });
-
-    VoipPushNotification.addEventListener('notification', notification => {
-      const {status, uuid} = notification;
-      if (status === VideoCallStatus.INITIALIZED) {
-        CallkeepHelper.configure(answerIncomingCall, endIncomingCall);
-      } else if (status === VideoCallStatus.DISCONNECTED) {
-        endIncomingCall();
-      }
-      VoipPushNotification.onVoipNotificationCompleted(uuid);
-    });
-
-    VoipPushNotification.addEventListener('didLoadWithEvents', events => {
-      // If loaded with notification received events
-      if (events.length >= 2) {
-        console.log('events:', JSON.stringify(events));
-        const status = events[0].data?.status || events[1].data?.status;
-        const uuid = events[0].data?.uuid || events[1].data?.uuid;
-        if (status === VideoCallStatus.INITIALIZED) {
-          CallkeepHelper.configure(answerIncomingCall, endIncomingCall);
-        } else if (status === VideoCallStatus.DISCONNECTED) {
-          endIncomingCall();
-        }
-        VoipPushNotification.onVoipNotificationCompleted(uuid);
-      }
-    });
-
-    // console.log('Registering VoIP token');
-    Platform.OS === 'ios' && VoipPushNotification.registerVoipToken();
-
-    return () => {
-      VoipPushNotification.removeEventListener('didLoadWithEvents');
-      VoipPushNotification.removeEventListener('register');
-      VoipPushNotification.removeEventListener('notification');
-    };
-  }, []);
+  const {numBgCallAccepted} = useNotification();
 
   return (
     <SafeAreaProvider>
---
+++ src/push_video/hooks/useNotification.tsx
@@ -0,0 +1,123 @@
+import {VideoCallStatus} from '@pushprotocol/restapi';
+import messaging from '@react-native-firebase/messaging';
+import {useCallback, useEffect, useState} from 'react';
+import {Platform} from 'react-native';
+import RNCallKeep, {InitialEvents} from 'react-native-callkeep';
+import VoipPushNotification from 'react-native-voip-push-notification';
+import CallkeepHelper from 'src/callkeep';
+import AppBadgeHelper from 'src/helpers/AppBadgeHelper';
+import MetaStorage from 'src/singletons/MetaStorage';
+import Notify from 'src/singletons/Notify';
+
+const handleAppNotificationBadge = async () => {
+  await AppBadgeHelper.setAppBadgeCount(0);
+};
+
+export const useNotification = () => {
+  const [numBgCallAccepted, setNumBgCallAccepted] = useState(0);
+
+  const answerIncomingCall = useCallback(() => {
+    MetaStorage.instance.setBackgroundCallAccepted(false);
+    setNumBgCallAccepted(prev => prev + 1);
+  }, []);
+
+  const endIncomingCall = useCallback(() => {
+    CallkeepHelper.endAllCall();
+  }, []);
+
+  // Background call handling for android
+  useEffect(() => {
+    RNCallKeep.addEventListener('answerCall', async () => {
+      if (Platform.OS === 'android') {
+        CallkeepHelper.backToForeground();
+        CallkeepHelper.endAllCall();
+      }
+      answerIncomingCall();
+    });
+
+    RNCallKeep.addEventListener('endCall', async () => {
+      endIncomingCall();
+    });
+  }, []);
+
+  // Background call handling for ios
+  useEffect(() => {
+    RNCallKeep.addEventListener(
+      'didLoadWithEvents',
+      // @ts-ignore Incorrect type definition
+      (events: InitialEvents) => {
+        console.log('didLoadWithEvents', events);
+        const answerCallEvent = events.find(
+          event => event.name === 'RNCallKeepPerformAnswerCallAction',
+        );
+        if (answerCallEvent) {
+          answerIncomingCall();
+        }
+      },
+    );
+
+    VoipPushNotification.addEventListener('register', token => {
+      console.log('Registering APN token:', token);
+      MetaStorage.instance.setApnsVoipToken(token);
+    });
+
+    VoipPushNotification.addEventListener('notification', notification => {
+      const {status, uuid} = notification as {
+        status: VideoCallStatus;
+        uuid: string;
+      };
+      if (status === VideoCallStatus.INITIALIZED) {
+        CallkeepHelper.configure(answerIncomingCall, endIncomingCall);
+      } else if (status === VideoCallStatus.DISCONNECTED) {
+        endIncomingCall();
+      }
+      VoipPushNotification.onVoipNotificationCompleted(uuid);
+    });
+
+    VoipPushNotification.addEventListener(
+      'didLoadWithEvents',
+      (events: any) => {
+        // If loaded with notification received events
+        if (events.length >= 2) {
+          console.log('events:', JSON.stringify(events));
+          const status = events[0].data?.status || events[1].data?.status;
+          const uuid = events[0].data?.uuid || events[1].data?.uuid;
+          if (status === VideoCallStatus.INITIALIZED) {
+            CallkeepHelper.configure(answerIncomingCall, endIncomingCall);
+          } else if (status === VideoCallStatus.DISCONNECTED) {
+            endIncomingCall();
+          }
+          VoipPushNotification.onVoipNotificationCompleted(uuid);
+        }
+      },
+    );
+
+    // console.log('Registering VoIP token');
+    Platform.OS === 'ios' && VoipPushNotification.registerVoipToken();
+
+    return () => {
+      VoipPushNotification.removeEventListener('didLoadWithEvents');
+      VoipPushNotification.removeEventListener('register');
+      VoipPushNotification.removeEventListener('notification');
+    };
+  }, []);
+
+  useEffect(() => {
+    // PUSH NOTIFICATIONS HANDLING
+    Notify.instance.requestDeviceToken(true);
+
+    // Listen to whether the token changes
+    const onTokenRefresh = messaging().onTokenRefresh(token => {
+      Notify.instance.saveDeviceToken(token, true); // true means it's a refresh
+    });
+
+    return () => {
+      onTokenRefresh;
+      handleAppNotificationBadge();
+    };
+  }, []);
+
+  return {
+    numBgCallAccepted,
+  };
+};
