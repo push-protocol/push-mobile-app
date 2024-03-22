1f2f07e71b41d9abc04a1e4542ca51f2beb4ee4a
kalashshah <202051096@iiitvadodara.ac.in>
2024-03-22 16:53:29 +0530

    chore: move sub/unsub to low lvl apis and add toasts

--- src/hooks/channel/useSubscriptions.tsx
+++ src/hooks/channel/useSubscriptions.tsx
@@ -1,8 +1,14 @@
 import type {UserSetting as PushUserSetting} from '@pushprotocol/restapi';
+import {channels} from '@pushprotocol/restapi';
+import {PushNotificationBaseClass} from '@pushprotocol/restapi/src/lib/pushNotification/pushNotificationBase';
+import {ENV} from '@pushprotocol/socket/src/lib/constants';
+import {useWalletConnectModal} from '@walletconnect/modal-react-native';
 import {useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {ToasterOptions} from 'src/components/indicators/Toaster';
 import {UserSetting} from 'src/components/sheets/NFSettingSheet';
 import {usePushApi} from 'src/contexts/PushApiContext';
+import {useToaster} from 'src/contexts/ToasterContext';
 import envConfig from 'src/env.config';
 import {caip10ToWallet} from 'src/helpers/CAIPHelper';
 import {
@@ -13,19 +19,18 @@ import {
   setLoadingSubscriptions,
   setSubscriptions,
 } from 'src/redux/channelSlice';
+import {getSigner} from 'src/walletconnect/chat/utils';
 
 const useSubscriptions = () => {
   const [loaded, setLoaded] = useState(false);
-  const {userPushSDKInstance, showUnlockProfileModal, readOnlyMode} =
-    usePushApi();
+  const {userPushSDKInstance, readOnlyMode} = usePushApi();
   const subscriptions = useSelector(selectSubscriptions);
   const dispatch = useDispatch();
+  const {isConnected, provider, open} = useWalletConnectModal();
+  const {toastRef} = useToaster();
 
   const subscribe = async (channel: string, settings?: UserSetting[]) => {
-    if (readOnlyMode) {
-      showUnlockProfileModal();
-      return;
-    }
+    const channelCaip = `eip155:${envConfig.CHAIN_ID}:${channel}`;
     const pushSettings: PushUserSetting[] | undefined = settings?.map(
       setting => {
         if (setting.type === 1) return {enabled: setting.user};
@@ -36,35 +41,112 @@ const useSubscriptions = () => {
           };
       },
     );
-    try {
-      const channelCaip = `eip155:${envConfig.CHAIN_ID}:${channel}`;
-      await userPushSDKInstance?.notification.subscribe(channelCaip, {
-        settings: pushSettings,
-        onSuccess: () => {
-          dispatch(
-            addChannelSubscription({
-              channel,
-              user_settings: settings ?? null,
-            }),
+    const onSuccess = () => {
+      dispatch(
+        addChannelSubscription({
+          channel,
+          user_settings: settings ?? null,
+        }),
+      );
+      toastRef.current?.showToast(
+        `Successfully subscribed to channel`,
+        '',
+        ToasterOptions.TYPE.GRADIENT_PRIMARY,
+      );
+    };
+    const onError = () => {
+      toastRef.current?.showToast(
+        `Error subscribing to channel`,
+        '',
+        ToasterOptions.TYPE.ERROR,
+      );
+    };
+
+    if (readOnlyMode) {
+      try {
+        if (isConnected && provider) {
+          const [signer, account] = await getSigner(provider);
+          const baseClass = new PushNotificationBaseClass(
+            signer,
+            envConfig.ENV as ENV,
+            account,
           );
-        },
-      });
-    } catch (e) {
-      console.error(e);
+          const settings = pushSettings
+            ? // @ts-ignore
+              baseClass.getMinimalUserSetting(pushSettings)
+            : null;
+          await channels.subscribeV2({
+            channelAddress: channelCaip,
+            signer: signer,
+            userAddress: account,
+            env: envConfig.ENV as ENV,
+            settings: settings,
+            onSuccess: onSuccess,
+            onError: onError,
+          });
+        } else {
+          await open();
+        }
+      } catch (e) {
+        console.error(e);
+      }
+      return;
+    } else {
+      try {
+        await userPushSDKInstance?.notification.subscribe(channelCaip, {
+          settings: pushSettings,
+          onSuccess: onSuccess,
+          onError: onError,
+        });
+      } catch (e) {
+        console.error(e);
+      }
     }
   };
 
   const unsubscribe = async (channel: string) => {
+    const channelCaip = `eip155:${envConfig.CHAIN_ID}:${channel}`;
+    const onSuccess = () => {
+      dispatch(removeChannelSubscription({channel}));
+      toastRef.current?.showToast(
+        `Successfully unsubscribed from channel`,
+        '',
+        ToasterOptions.TYPE.GRADIENT_PRIMARY,
+      );
+    };
+
+    const onError = () => {
+      toastRef.current?.showToast(
+        `Error unsubscribing from channel`,
+        '',
+        ToasterOptions.TYPE.ERROR,
+      );
+    };
+
     if (readOnlyMode) {
-      showUnlockProfileModal();
+      try {
+        if (isConnected && provider) {
+          const [signer, account] = await getSigner(provider);
+          await channels.unsubscribeV2({
+            channelAddress: channelCaip,
+            signer,
+            userAddress: account,
+            env: envConfig.ENV as ENV,
+            onSuccess: onSuccess,
+            onError: onError,
+          });
+        } else {
+          await open();
+        }
+      } catch (e) {
+        console.error(e);
+      }
       return;
     }
     try {
-      const channelCaip = `eip155:${envConfig.CHAIN_ID}:${channel}`;
       await userPushSDKInstance?.notification.unsubscribe(channelCaip, {
-        onSuccess: () => {
-          dispatch(removeChannelSubscription({channel}));
-        },
+        onSuccess: onSuccess,
+        onError: onError,
       });
     } catch (e) {}
   };
